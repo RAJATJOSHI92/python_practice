b806580b8deec727438c6c32b07a14c38d180109
Rajat <rajat.joshi087@gmail.com>
2024-11-06 13:50:07 +0530

    commit11

---
+++ Basic/basic.py
@@ -0,0 +1,35 @@
+import random
+cars=["rajat","joshi"]
+"""
+                dvehdbbdjekddedkdhldhdgheqdgwd
+w               lqdnw
+[                   hw;qf
+w           jf'wldfk
+'w          jf
+            wq  fjw'
+lf
+wkf"""
+print(cars)
+cars[0]="joshiiiuuuuuugggggg"
+print(cars)
+cars.append("great")
+print(cars)
+cars.pop(0)
+print(cars)
+carss=["hello"]
+carss.copy()
+print(carss)
+
+
+r=5
+s="rajat"
+S='rajat'
+list=[1,2,3,4,5,6]
+set={1,2,3,4}
+t=(1,2,3,4)
+dict={1:"rajat",2:"joshi"}
+
+print(type(s),type(r),type(S),type(list),type(set),type(t),type(dict))
+
+
+print(random.randrange(1,23))
